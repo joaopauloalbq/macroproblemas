cc141755ecf8cdbe5a030d1971e51263a5a43d0a
João Paulo Albuquerque <joaopauloalbu@gmail.com>
2023-11-15 02:31:34 -0300

    Add select box and bar chart

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
+import plotly.express as px
 from openpyxl import load_workbook
 
 def getKey(dic, valor):
@@ -19,13 +21,17 @@ def getCities(sheet, linha, coluna):
 
     return cidades
 
-def main():        
+def main():
+    st.header("Painel de Acompanhamento, Monitoramento e Avaliação dos Indicadores da Saúde do Estado")
+    st.subheader("Diretoria de Unidade de Planejamento (DUP)")
+    st.divider()
     planilha = st.file_uploader('Selecione a planilha', type='xlsx')
+    st.divider()
 
     if planilha is not None:
         workbook = load_workbook(planilha)
 
-        dataDictionary = {"MACROPROBLEMA":[], "CIDADE":[], "RESULTADO":[], "TOTAL":[], "PRIORIDADE":[], "MACROREGIAO":[]}
+        dataDictionary = {"MACROPROBLEMA":[], "CIDADE":[], "AVALIAÇÃO":[], "TOTAL":[], "PRIORIDADE":[], "MACROREGIAO":[]}
         for sheetname in workbook.sheetnames:
             sheet = workbook[sheetname]
 
@@ -38,23 +44,50 @@ def main():
                 coluna = 2
                 total = sheet.cell(row=linha, column=getKey(cidades,"TOTAL")).value
                 priodade = sheet.cell(row=linha, column=getKey(cidades,"PRIORIDADES SANITÁRIAS")).value
-                resultado = sheet.cell(row=linha, column=coluna).value
-                while resultado is not None and cidades[coluna] != "TOTAL":
-                    
+                avaliacao = sheet.cell(row=linha, column=coluna).value
+                while avaliacao is not None and cidades[coluna] != "TOTAL":
+
                     dataDictionary["MACROPROBLEMA"].append(macroProblema)
                     dataDictionary["CIDADE"].append(cidades[coluna])
-                    dataDictionary["RESULTADO"].append(resultado)
+                    dataDictionary["AVALIAÇÃO"].append(avaliacao)
                     dataDictionary["TOTAL"].append(total)
                     dataDictionary["PRIORIDADE"].append(priodade)
                     dataDictionary["MACROREGIAO"].append(sheet.title)
 
                     coluna += 1
-                    resultado = sheet.cell(row=linha, column=coluna).value
+                    avaliacao = sheet.cell(row=linha, column=coluna).value
 
                 linha += 1
                 macroProblema = sheet.cell(row=linha, column=1).value
 
         df = pd.DataFrame(dataDictionary)
-        st.write(df)
+
+        macroRegiaoOptions = np.append(['TODAS'], df["MACROREGIAO"].unique())
+        macroRegiaoEscolhida = st.selectbox(label="Macro Região", options=macroRegiaoOptions)
+
+        if macroRegiaoEscolhida == 'TODAS':
+            macroProblemaOptions = np.append(['TODOS'], df["MACROPROBLEMA"].unique())
+            macroProblemaEscolhido = st.selectbox(label="Macro Problema", options=macroProblemaOptions)
+            # st.table(macroProblemaOptions)
+        else:
+            macroProblemaDaMacroRegiao = np.append(['TODOS'], df.loc[(df['MACROREGIAO'] == macroRegiaoEscolhida)]["MACROPROBLEMA"].unique())
+            macroProblemaEscolhido = st.selectbox(label="Macro Problema", options=macroProblemaDaMacroRegiao)
+            # st.table(macroProblemaDaMacroRegiao)
+
+
+        if macroProblemaEscolhido:
+            if macroProblemaEscolhido == 'TODOS':
+                if macroRegiaoEscolhida == 'TODAS':
+                    df_filtered = df
+                else:
+                    df_filtered = df.loc[(df["MACROREGIAO"] == macroRegiaoEscolhida)]
+            else:
+                df_filtered = df.loc[(df["MACROPROBLEMA"] == macroProblemaEscolhido)]
+            
+            # st.write(df_filtered)
+            bar = px.bar(df_filtered, x='CIDADE', y ='AVALIAÇÃO', color='MACROPROBLEMA')
+            bar.update_traces(hoverlabel_align='left', selector=dict(type='bar'))
+            st.plotly_chart(bar, use_container_width=True)
+
 
 main()
